3617d459bd9c0ad4dd261011cfd9a9db659b2f20
tarravechiarj <tarravechiarj@appstate.edu>
2018-03-02 22:27:05 -0500

    got CTC mode working

--- TimerTest.c
+++ TimerTest.c
@@ -8,29 +8,39 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-/* ONTIME is 75ms; PERIOD is ~333.3ms; 
-   START is Timer register 1's TOP value - PERIOD */
+/* ONTIME is 75ms; PERIOD is ~333.3ms; */
 #define ONTIME  1171
 #define PERIOD  5208
-#define START   (65535 - PERIOD)
 
+/* ontime = 3s, period = 4s (for testing) */
+//#define ONTIME  46875
+//#define PERIOD  62500
+
+/* pre-scaling for clk/1024 */
+#define PRE_SCALING ((1 << CS12) | (1 << CS10))
 #define OUTPUT_PIN  (1 << PINB7)
-#define PRE_SCALING ((1 << CS12) | (1 << CS10)) // clk/1024
 
-/* initialize timer 1 for normal mode 
- */
+/* START is Timer register 1's TOP value - PERIOD */
+#define NORMAL_START
+
+/* 0 = normal; 1 = CTC; 2 = phase and frequency correct PWM mode */
+#define MODE 1
+
+/* settings for leapfrogging in normal mode */
+#if MODE == 0
+
 void setup() {
-	cli();         // disable global interrupts	
+	cli();         // disable global interrupts
 	TCCR1A = 0;    // set entire TCCR1A register to 0
 	TCCR1B = 0;    // set entire TCCR1A register to 0
+
+	TCNT1 = NORMAL_START;
+	TCCR1B |= PRE_SCALING;
 
 	TIMSK1 |= (1 << TOIE1);		// enable Timer1 overflow interrupt
 	TIMSK1 |= (1 << OCIE1A);	// enable Timer1 compare A interrupt
 	
-	TCNT1 = START;
-	TCCR1B |= PRE_SCALING;
-	
-	OCR1A = START + ONTIME;		//set up match for 75ms after start value
+	OCR1A = NORMAL_START + ONTIME;	//set up match for 75ms after start value
 	DDRB |= OUTPUT_PIN;
 	PORTB |= OUTPUT_PIN;		// LED starts on
 	
@@ -42,6 +52,36 @@ ISR(TIMER1_COMPA_vect) {
 }
 
 ISR(TIMER1_OVF_vect) {
-	PORTB ^= OUTPUT_PIN; // turn on light again
-	TCNT1 = START;		 // reload the timer
+	PORTB ^= OUTPUT_PIN;  // turn on light again
+	TCNT1 = NORMAL_START; // reload the timer
 }
+#endif
+
+/* settings for CTC mode */
+#if MODE == 1
+
+void setup() {
+	cli();         // disable global interrupts
+	TCCR1A = 0;    // set entire TCCR1A register to 0
+	TCCR1B = 0;    // set entire TCCR1A register to 0
+
+	TCCR1B |= (1 << WGM13) | (1 << WGM12);	// CTC mode
+	TCCR1B |= PRE_SCALING;
+	
+	TIMSK1 |= (1 << OCIE1A);	// enable Timer1 compare A interrupt
+	TIMSK1 |= (1 << ICIE1);	    // enable Timer1 input capture interrupt
+	
+	ICR1 = PERIOD;				// set TOP value to PERIOD
+	OCR1A = ONTIME;				// set up match for 75ms
+	DDRB |= OUTPUT_PIN;
+	PORTB |= OUTPUT_PIN;		// LED starts on
+	
+	sei(); // enable global interrupts
+}
+
+ISR(TIMER1_COMPA_vect) {
+	PORTB ^= OUTPUT_PIN; // toggle LED state
+}
+
+ISR_ALIAS(TIMER1_CAPT_vect, TIMER1_COMPA_vect);
+#endif
